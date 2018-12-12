e558c1fb4a2cdc51134ee4739fcf05f3392242b4
Nathaniel McCallum <npmccallum@redhat.com>
2018-12-12 10:11:08 -0500

    Move source files to src subdirectory

--- tests/echo.c
+++ tests/echo.c
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include "../tlssock.h"
+#include "../src/tlssock.h"
 
 #include <arpa/inet.h>
 #include <sys/types.h>
