b65af45447b9cd71be4cd758013e750b4fc805ec
Nathaniel McCallum <npmccallum@redhat.com>
2019-01-23 18:36:43 -0500

    Move src/ to lib/
    This is preparation for adding a nc-like binary.

--- tests/echo.c
+++ tests/echo.c
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include "../src/tlssock.h"
+#include "../lib/tlssock.h"
 
 #include <arpa/inet.h>
 #include <sys/types.h>
