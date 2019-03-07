33ed94394305f5a3d3a011847a7e8cae7ec96b0a
Robbie Harwood <rharwood@redhat.com>
2019-03-07 18:15:20 -0500

    Move _GNU_SOURCE definition into a compiler flag

--- tests/run.c
+++ tests/run.c
@@ -19,7 +19,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#define _GNU_SOURCE
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
